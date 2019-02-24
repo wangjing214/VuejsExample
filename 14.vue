c794bd78525574e85e4e0ccecccd4b6705803beb
wangjing <1970960049@qq.com>
2019-02-24 23:45:49 +0800

    add

---
+++ 14.vue
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>filter 数组更新</title>
+<style>
+    [v-cloak] {display:none !important;}
+</style>
+</head>
+<body>
+    <div id='app' v-cloak>
+        <ul>
+            <template v-for="book in books">
+                <li>书名：{{ book.name }}</li>
+                <li>作者：{{ book.author }}</li>
+            </template>
+        </ul>
+    </div>
+    <script src="https://unpkg.com/vue/dist/vue.min.js"></script>
+    <script>
+        var app = new Vue({
+            el: '#app',
+            data: {
+                books: [
+                    {
+                        name: 'book 111',
+                        author: 'wj'
+                    },
+                    {
+                        name: 'book 22',
+                        author: 'wj22'
+                    },
+                    {
+                        name: 'haha 33',
+                        author: 'wj33'
+                    },
+                ]
+            }
+        })
+
+        app.books = app.books.filter (function(item) {
+            return item.name.match(/book/);
+        });
+    </script>
+</body>
+</html>
---
+++ 15.vue
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>++1 ++2 单击数</title>
+<style>
+    [v-cloak] {display:none !important;}
+</style>
+</head>
+<body>
+    <div id='app' v-cloak>
+        点击次数： {{ counter }}
+        <button @click="counter+=2">+2</button>
+        点击次数： {{ counter2 }}
+        <button @click="handleAdd()">+1</button>
+        <button @click="handleAdd(10)">+10</button>
+    </div>
+    <script src="https://unpkg.com/vue/dist/vue.min.js"></script>
+    <script>
+        var app = new Vue({
+            el: '#app',
+            data: {
+                counter: 0,
+                counter2: 0
+            },
+            methods: {
+                handleAdd: function (num) {
+                    num = num || 1;
+                    this.counter2 += num;
+                }
+            }
+        })
+    </script>
+</body>
+</html>
